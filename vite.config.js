c677aabc631312fcea0a642eb0a906645d5009b2
SFJT-97 <santiagojuarez.dev@gmail.com>
2025-11-24 13:15:32 -0300

    commit supabase fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  build: {
-    outDir: 'dist'
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    build: {
+      outDir: 'dist'
+    },
+    define: {
+      'import.meta.env.SUPABASE_URL': JSON.stringify(env.SUPABASE_URL),
+      'import.meta.env.SUPABASE_ANON_KEY': JSON.stringify(env.SUPABASE_ANON_KEY),
+    },
+  }
 })
